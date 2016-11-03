2f2bb85b9668a0aa3af18ce9487cb8277d07424e
Dang <letatdang@gmail.com>
2016-11-03 16:51:28 +0700

    Interface and functionality
    I designed the interface and write some classes in package
    "functionnality"

---
+++ Maze Battle Game/src/functionality/Bomb.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package functionality;
+
+/**
+ *
+ * @author admin
+ */
+public class Bomb {
+    private String Direction;
+    private int Speed;
+    private int Strength;
+}
---
+++ Maze Battle Game/src/functionality/Player.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package functionality;
+
+import javax.swing.JLabel;
+
+/**
+ *
+ * @author admin
+ */
+public class Player {
+    private String Name;
+    private String Direction;
+    private int xPos;
+    private int yPos;
+    private JLabel photo;
+    private int score;
+    private Bomb ammunition;
+}
